1cce6f4f1a36f57042c6810a618e22aae6dcb799
Jean-Sebastien Goupil <contact@jsgoupil.com>
2021-05-19 13:08:52 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,34 @@
+<p align="center"><a href="https://www.barcodebakery.com" target="_blank">
+    <img src="https://www.barcodebakery.com/images/BCG-Logo-SQ-GitHub.svg">
+</a></p>
+
+This example repository will allow you to generate QRCode barcodes. You can find more information on our [Barcode Bakery website][1].
+
+This is based on the [barcode QRCode][2] library.
+
+The library is not free, you must [purchase a license][3] in order to obtain it.
+
+Installation
+------------
+
+* Download the library on our [website][4], and follow our [developer's guide][5].
+
+This repository is already setup to run the examples. However, you will need to purchase the library first.
+
+Then, you simply need to run:
+```bash
+$ npm install
+```
+
+Then call a file [app-qrcode.js][6] with Node:
+```bash
+node src/app-qrcode.js
+```
+
+
+[1]: https://www.barcodebakery.com
+[2]: https://www.barcodebakery.com/en/docs/nodejs/barcode/qrcode/api
+[3]: https://www.barcodebakery.com/en/purchase
+[4]: https://www.barcodebakery.com/en/docs/nodejs/barcode/qrcode/download
+[5]: https://www.barcodebakery.com/en/docs/nodejs/guide
+[6]: https://github.com/barcode-bakery/example-nodejs-qrcode/blob/master/src/app-azted.js
---
+++ src/app-qrcode.js
@@ -0,0 +1,66 @@
+﻿import { createServer } from 'http';
+import { parse } from 'querystring';
+import { BCGColor, BCGDrawing, BCGFont, BCGLabel } from '@barcode-bakery/barcode-common';
+import { BCGqrcode } from '@barcode-bakery/barcode-qrcode';
+
+let defaultText = 'QRCode';
+
+// Loading Font
+let font = new BCGFont('Arial', 18);
+
+// The arguments are R, G, B for color.
+let colorBlack = new BCGColor(0, 0, 0);
+let colorWhite = new BCGColor(255, 255, 255);
+
+let getDrawing = function (text) {
+    let drawException = null,
+        barcode = null;
+    try {
+        // Label, this part is optional
+        let label = new BCGLabel();
+        label.setFont(font);
+        label.setPosition(BCGLabel.Position.Bottom);
+        label.setAlignment(BCGLabel.Alignment.Center);
+        label.setText(text);
+
+        let code = new BCGqrcode();
+        code.setScale(3);
+        code.setSize(BCGqrcode.Size.Full);
+        code.setErrorLevel('M');
+        code.setMirror(false);
+        code.setQuietZone(true);
+        code.setBackgroundColor(colorWhite); // Color of spaces
+        code.setForegroundColor(colorBlack); // Color of bars
+
+        code.addLabel(label);
+
+        code.parse(text); // Text
+        barcode = code;
+    } catch (exception) {
+        drawException = exception;
+    }
+
+    let drawing = new BCGDrawing(barcode, colorWhite);
+    if (drawException) {
+        drawing.drawException(drawException);
+    }
+
+    return drawing;
+};
+
+/*
+// This is how you would save to a file.
+let drawing = getDrawing(defaultText);
+drawing.save("image.png", BCGDrawing.ImageFormat.Png, function () {
+    console.log("Done.");
+});
+*/
+
+createServer(function (request, response) {
+    let drawing = getDrawing(parse(request.url).query?.toString() || defaultText);
+    drawing.toBuffer(BCGDrawing.ImageFormat.Png, function (err, buffer) {
+        response.writeHead(200, { 'Content-Type': 'image/png' });
+        response.end(buffer);
+    });
+}).listen(8124);
+console.log('Server running at http://127.0.0.1:8124/');
